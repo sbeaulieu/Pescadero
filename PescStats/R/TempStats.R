6910f48bf710f79e580e3c4926364a372b631589
Mary Toner <tonermabc@gmail.com>
2018-06-21 18:32:41 -0400

    added optional variable for titling barplot

--- PescStats/R/TempStats.R
+++ PescStats/R/TempStats.R
@@ -9,8 +9,9 @@
 #'@param x file name of the csv containing the desired temperature data (eg: "ISS_001_log.csv")
 #'@param t the timestamp of the desired start time, including the year, month, day, hour, minute, and second. (eg. 2017-10-31T19:00:12.656Z)
 #'@param d the duration, in minutes, of the data one wishes to analyze
+#'@param title the desired title of the barplot, in quotes
 #'@examples
-#'          TempStats("ISS_001_log.csv","2017-10-31 14:48:00",10)
+#'          TempStats("ISS_001_log.csv","2017-10-31 14:48:00",10,"ISS1 Bullseye")
 #'Temperature Data from 2017-10-31 14:48:00 to 2017-10-31 14:58:00
 #'Summary Statistics:
 #' mean = 40.627
@@ -20,7 +21,7 @@
 #' standard deviation = 3.482
 #' Standard Error of the Mean = 0.176
 #'  
-#'          TempStats("temp_log_002.csv","2017-10-31T20:53:26.428Z",15)
+#'          TempStats("temp_log_002.csv","2017-10-31T20:53:26.428Z",15,"ISS2 Away")
 #'Temperature Data from 2017-10-31 20:53:26 to 2017-10-31 21:08:26
 #'Summary Statistics:
 #'  mean = 9.511
@@ -31,7 +32,7 @@
 #'  Standard Error of the Mean = 0.135
 #'@export
 
-TempStats<-function(x,t,d){
+TempStats<-function(x,t,d,title){
   ISS<-read.csv(x)
   ISS$time<-ymd_hms(ISS$time)
   start<-ymd_hms(t)
@@ -39,5 +40,9 @@ TempStats<-function(x,t,d){
   ISS_duration<-subset(ISS, time>start & time<end)
   writeLines(paste("Temperature Data from",start,"to",end))
   sum_stats(ISS_duration$temperature)
-  barplot(ISS_duration$temperature, xlab=paste("Temps for",start,"to",end))
+ if (missing(title))
+  title<-NULL
+ else
+   title=title
+  barplot(ISS_duration$temperature, main=title, xlab=paste("Temps for",start,"to",end))
 }
